10feb42d59f9f75cd102a1ed12a1c230486b2252
nsay <narithsay@gmail.com>
2025-09-26 15:38:59 -0400

    redesign buttons

--- Client/src/app/chat/chat.ts
+++ Client/src/app/chat/chat.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -19,6 +20,7 @@ import { ChatService } from '../services/chat.service';
     CommonModule, 
     FormsModule, 
     MatIconModule, 
+    MatButtonModule,
     MatRadioModule, 
     MatCheckboxModule, 
     MatProgressBarModule, 
